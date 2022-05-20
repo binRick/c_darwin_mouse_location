ee745a8d60a58154129a5c79c2d6854b7e0b7834
rick <rick@ricks-MacBook-Air.local>
2022-05-20 18:28:36 -0400

    refactor build script to support arbitrary test files

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,25 @@
 set -eou pipefail
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd ../
+DEBUG=${DEBUG:-0}
 
 [[ -d deps ]] || clib i -c -q
 
 [[ -d bin ]] && rm -rf bin
 mkdir -p bin
 
+get_test_files() {
+	\find tests -type f -name "*.c" | \sort -u
+}
+
+build_test_file() {
+	cmd="gcc $1 \
+		-framework ApplicationServices \
+        -o bin/$(basename $1 .c)"
+	[[ "$DEBUG" == 1 ]] && ansi >&2 --yellow --bg-black --italic "$cmd"
+	eval "$cmd"
+
+}
 build_test() {
 	gcc tests/test-darwin-mouse-location$1.c \
 		-framework ApplicationServices \
@@ -15,6 +28,15 @@ build_test() {
 
 }
 
-build_test 0
-build_test 1
-build_test 2
+build_test_files() {
+	while read -r test_file; do
+		build_test_file "$test_file" &
+	done < <(get_test_files)
+	wait
+}
+
+main() {
+	build_test_files
+}
+
+main
